9846de06cc7cd3e36c0977f4c808dcd5867124df
0xB9 <0xB9@pm.me>
2024-03-23 01:21:32 -0400

    Support left mouse clicks
    This closes #1

--- Autoclick/Form1.cs
+++ Autoclick/Form1.cs
@@ -9,6 +9,8 @@ namespace Autoclick
         [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
         public static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint cButtons, uint dwExtraInfo);
 
+        private const uint MOUSEEVENTF_LEFTDOWN = 0x0002;
+        private const uint MOUSEEVENTF_LEFTUP = 0x0004;
         private const uint MOUSEEVENTF_RIGHTDOWN = 0x0008;
         private const uint MOUSEEVENTF_RIGHTUP = 0x0010;
 
@@ -44,10 +46,20 @@ namespace Autoclick
 
         private void AutoClick(object sender, EventArgs e)
         {
-            // Simulate right mouse button down
-            mouse_event(MOUSEEVENTF_RIGHTDOWN, 0, 0, 0, 0);
-            // Simulate right mouse button up
-            mouse_event(MOUSEEVENTF_RIGHTUP, 0, 0, 0, 0);
+            if (leftMouseButton.Checked)
+            {
+                // Simulate left mouse button down
+                mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+                // Simulate left mouse button up
+                mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+            }
+            else if (rightMouseButton.Checked)
+            {
+                // Simulate right mouse button down
+                mouse_event(MOUSEEVENTF_RIGHTDOWN, 0, 0, 0, 0);
+                // Simulate right mouse button up
+                mouse_event(MOUSEEVENTF_RIGHTUP, 0, 0, 0, 0);
+            }        
         }
 
         // Only allow numbers and backspace
