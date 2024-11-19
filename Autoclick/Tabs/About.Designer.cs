a0765a6b29a9b84b097a22d341751a60ce71ba88
0xB9 <0xB9@pm.me>
2024-11-19 01:06:56 -0500

    Add about tab

---
+++ Autoclick/Tabs/About.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace Autoclick.Tabs
+{
+    partial class About
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(155, 90);
+            label1.Name = "label1";
+            label1.Size = new Size(46, 16);
+            label1.TabIndex = 0;
+            label1.Text = "About";
+            // 
+            // About
+            // 
+            AutoScaleDimensions = new SizeF(8F, 16F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(40, 9, 79);
+            Controls.Add(label1);
+            Font = new Font("Century Gothic", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            Name = "About";
+            Size = new Size(379, 213);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+    }
+}
