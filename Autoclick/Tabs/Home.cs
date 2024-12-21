f0c023607d6a50e120b8391171661dbf53ccfb92
0xB9 <0xB9@pm.me>
2024-12-20 19:25:28 -0500

    Fix hotkeys
    Related to #16

--- Autoclick/Tabs/Home.cs
+++ Autoclick/Tabs/Home.cs
@@ -13,6 +13,15 @@ namespace Autoclick.Tabs
 {
     public partial class Home : UserControl
     {
+        [DllImport("user32.dll")]
+        private static extern bool RegisterHotKey(IntPtr hWnd, int id, uint fsModifiers, uint vk);
+        [DllImport("user32.dll")]
+        private static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+        private const int WM_HOTKEY = 0x0312;
+        private const int HOTKEY_START = 1;
+        private const int HOTKEY_STOP = 2;
+
         [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
         public static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint cButtons, uint dwExtraInfo);
 
@@ -27,6 +36,11 @@ namespace Autoclick.Tabs
             InitializeComponent();
             autoclickTimer = new System.Windows.Forms.Timer();
             autoclickTimer.Tick += new EventHandler(AutoClick);
+
+            this.ParentForm.FormClosing += ParentForm_FormClosing;
+
+            RegisterHotKey(this.Handle, HOTKEY_START, 0, (uint)Keys.F7);
+            RegisterHotKey(this.Handle, HOTKEY_STOP, 0, (uint)Keys.F8);
         }
 
         // Start auto-clicking
@@ -75,19 +89,30 @@ namespace Autoclick.Tabs
         }
 
         // Use F7/F8 keys to start/stop auto-clicking
-        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        protected override void WndProc(ref Message m)
         {
-            if (keyData == Keys.F7)
+            base.WndProc(ref m);
+
+            if (m.Msg == WM_HOTKEY)
             {
-                startButton_Click(this, EventArgs.Empty);
-                return true;
+                int id = m.WParam.ToInt32();
+
+                if (id == HOTKEY_START)
+                {
+                    startButton_Click(this, EventArgs.Empty);
+                }
+                else if (id == HOTKEY_STOP)
+                {
+                    stopButton_Click(this, EventArgs.Empty);
+                }
             }
-            else if (keyData == Keys.F8)
-            {
-                stopButton_Click(this, EventArgs.Empty);
-                return true;
-            }
-            return base.ProcessCmdKey(ref msg, keyData);
+        }
+
+        // Unregister hotkeys
+        private void ParentForm_FormClosing(FormClosingEventArgs e)
+        {
+            UnregisterHotKey(this.Handle, HOTKEY_START);
+            UnregisterHotKey(this.Handle, HOTKEY_STOP);
         }
 
         private void AutoClick(object? sender, EventArgs e)
